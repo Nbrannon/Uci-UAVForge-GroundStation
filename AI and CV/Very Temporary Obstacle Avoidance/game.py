04affaaf54ba8074b188a32e6feed3a20b4142ae
Martin Asenov <masenov@uci.edu>
2014-11-23 18:38:30 -0800

    Fixed bugs, finished one obstacle avoidance, started working on multiple obstacle avoidance

--- AI and CV/Very Temporary Obstacle Avoidance/game.py
+++ AI and CV/Very Temporary Obstacle Avoidance/game.py
@@ -6,15 +6,19 @@ import math
 import numpy as np
 
 
+def pointDistance(p1,p2):
+    return math.sqrt(math.pow(p1[0]-p2[0],2) + math.pow(p1[1]-p2[1],2))
+
 def distance(x1,y1,x2,y2):
     distx = x2 - x1
     disty = y2 - y1
     return (distx, disty)
 
 def length(vector): # get length (used for normalize)
-        return math.sqrt((vector[0]**2 + vector[1]**2)) 
+        return math.sqrt((vector[0]**2 + vector[1]**2))
 
-def normalize(vector): # turn a total value into a constant ammount (hard to describe)
+
+def normalize(vector): # turn a total value into a constant amount (hard to describe)
                          # (0,50) becomes(0.0,1.0)
                          # (50,50) becomes (0.707, 0.707)
     l = length(vector)
@@ -22,14 +26,45 @@ def normalize(vector): # turn a total value into a constant ammount (hard to des
         return (vector[0] / l, vector[1] / l)
     return ([1,1])
 
-def distance_check(self, dist):
-    dist_x = dist[0] ** 2 # gets absolute value of the x distance
-    dist_y = dist[1] ** 2 # gets absolute value of the y distance
-    t_dist = dist_x + dist_y # gets total absolute value distance
-    speed = self.speed ** 2 # gets aboslute value of the speed
+def calculateDelta(obstacle):
+    # We have to subtract tree x and y because our circle does not have coordinates (0,0)
+    px1 = quadcopterc[0] - obstacle.centerx
+    py1 = quadcopterc[1] - obstacle.centery
+    px2 = goal[0] - obstacle.centerx
+    py2 = goal[1] - obstacle.centery
+    dx = px2 - px1
+    dy = py2 - py1
+    dr = math.sqrt(math.pow(dx,2) + math.pow(dy,2))
+    D2 = px1*py2 - px2*py1
+    delta = R*R*dr*dr - D2*D2
+    return delta
 
-    if t_dist < (speed): # read function description above
-        return True
+def calculateTangentLines(obstacle):
+    D = math.sqrt(math.pow(obstacle.centerx - quadcopterc[0], 2) + math.pow(obstacle.centery - quadcopterc[1],2))
+    L = math.sqrt(math.fabs(math.pow(D,2) - math.pow(R,2)))
+    h = R*L/D
+    a = (math.pow(L,2) - math.pow(R,2) + math.pow(D,2))/(2*D)
+    x2 = quadcopterc[0] + a * (x1 - quadcopterc[0]) / D
+    y2 = quadcopterc[1] + a * (y1 - quadcopterc[1]) / D
+    x31 = x2 - h * (y1 - quadcopterc[1]) / D
+    y31 = y2 + h * (x1 - quadcopterc[0]) / D
+    x32 = x2 + h * (y1 - quadcopterc[1]) / D
+    y32 = y2 - h * (x1 - quadcopterc[0]) / D
+    return (x31,y31,x32,y32)
+
+
+def updatePoint(x,y):
+    try:
+        x += (x - quadcopter.centerx)/length([x-quadcopter.centerx,y-quadcopter.centery])
+    except ZeroDivisionError:
+        pass
+    try:
+        y += (y - quadcopter.centery)/length([x-quadcopter.centerx,y-quadcopter.centery])
+    except ZeroDivisionError:
+        pass
+    return (x,y)
+
+
 
 
 pygame.init()
@@ -38,100 +73,119 @@ size = width, height = 1200, 800
 
 black = 250, 250, 250
 goal = (800, 400)
-
 ball = pygame.image.load("ball.bmp")
 quadcopter = ball.get_rect()
-ball2 = pygame.image.load("tree.png")
-tree = ball2.get_rect()
-tree = tree.move(300,150)
+quadcopterc = [1.0, 1.0]
+
+pic = pygame.image.load("tree2.png")
+tree = pic.get_rect()
+#tree = tree.move(-tree.width/2,-tree.height/2)
 R = tree.width/2*math.sqrt(2)
+
+points = [(410,400),(850,570),(430,200), (270,300), (900, 650)]
+obstacles = []
+obstacles_bucket = []
+
+
+for point in points:
+    obstacle = pic.get_rect()
+    obstacle = obstacle.move(-obstacle.width/2 + point[0],-obstacle.height/2 + point[1])
+    obstacles.append([obstacle,1])
+
+for bucket in obstacles:
+    if (bucket[1]==0):
+        continue
+    temp_bucket = [bucket[0]]
+    for obstacle in temp_bucket:
+        try:
+            i = obstacles.index([obstacle,1])
+            obstacles[i][1] = 0
+        except:
+            pass
+        for neighbour in obstacles:
+            if(neighbour[1]==0):
+                continue
+            if(obstacle==neighbour[0]):
+                continue
+            if(pointDistance(obstacle.center,neighbour[0].center)>2*R):
+                continue
+            temp_bucket.append(neighbour[0])
+            try:
+                i = obstacles.index([neighbour[0],1])
+                obstacles[i][1] = 0
+            except:
+                pass
+    obstacles_bucket.append(temp_bucket)
+
+print(obstacles_bucket)
+
+
+
+
+
+
+
+
 x1 = tree.centerx
 y1 = tree.centery
-slope1 = 1
-slope2 = 1
-slope3 = 1
 direction=([1,1])
+speed = 2
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
 
-    px1 = quadcopter.centerx - tree.centerx
-    py1 = quadcopter.centery - tree.centery
-    px2 = goal[0] - tree.centerx
-    py2 = goal[1] - tree.centery
-    dx = px2 - px1
-    dy = py2 - py1
-    dr = math.sqrt(math.pow(dx,2) + math.pow(dy,2))
-    D2 = px1*py2 - px2*py1
-    delta = R*R*dr*dr - D2*D2
+    # Checking if our path is clear using circle intersection
+    # More info - http://mathworld.wolfram.com/Circle-LineIntersection.html
+
+    delta = calculateDelta(tree)
+   # delta2 = calculateDelta(tree2)
 
 
-    speed = 2
+    # Calculating the tangent lines
 
-    D = math.sqrt(math.pow(tree.centerx - quadcopter.centerx, 2) + math.pow(tree.centery - quadcopter.centery,2))
-    L = math.sqrt(math.fabs(math.pow(D,2) - math.pow(R+15,2)))
-    x0 = quadcopter.centerx
-    y0 = quadcopter.centery
-    h = R*L/D
-    a = (math.pow(L,2) - math.pow(R+15,2) + math.pow(D,2))/(2*D)
-    x2 = x0 + a * (x1 - x0) / D
-    y2 = y0 + a * (y1 - y0) / D
-    x33 = x2 - h * (y1 - y0) / D
-    y33 = y2 + h * (x1 - x0) / D
-    x34 = x2 + h * (y1 - y0) / D
-    y34 = y2 - h * (x1 - x0) / D
-    try:
-        x31 = x33 + (x33 - quadcopter.centerx)/length([x33-quadcopter.centerx,y33-quadcopter.centery])*70
-    except ZeroDivisionError:
-        x31 = x33
-    try:
-        y31 = y33 + (y33 - quadcopter.centery)/length([x33-quadcopter.centerx,y33-quadcopter.centery])*70
-    except:
-        y31 = y33
-    try:
-        x32 = x34 + (x34 - quadcopter.centerx)/length([x34-quadcopter.centerx,y34-quadcopter.centery])*70
-    except:
-        x32 = x34
-    try:
-        y32 = y34 + (y34 - quadcopter.centery)/length([x34-quadcopter.centerx,y34-quadcopter.centery])*70
-    except:
-        y32 = y34
-    if(goal[0] - quadcopter.centerx != 0):
-        slope1 = math.fabs((float(goal[1])-quadcopter.centery)/(goal[0] - quadcopter.centerx))
-    if(x31 - quadcopter.centerx != 0):
-        slope2 = math.fabs((float(y31)-quadcopter.centery)/(x31 - quadcopter.centerx))
-    if(x32 - quadcopter.centerx != 0):
-        slope3 = math.fabs((float(y32)-quadcopter.centery)/(x32 - quadcopter.centerx))
+
+    (x31,y31,x32,y32) = calculateTangentLines(tree)
+   # (x312,y312,x322,y322) = calculateTangentLines(tree2)
+
     if (delta>0):
-        if(math.sqrt(math.pow(x31-goal[0], 2) + math.pow(y31-goal[1], 2)) > math.sqrt(math.pow(x32-goal[0], 2) + math.pow(y32-goal[1], 2))):
-            direction = normalize(distance(quadcopter.centerx,quadcopter.centery,x32,y32))
-            quadcopter.centerx += speed*direction[0]
-            quadcopter.centery += speed*direction[1]
+        if(length([x31-goal[0],y31-goal[1]]) > length([x32-goal[0], y32-goal[1]])):
+            (x32,y32) = updatePoint(x32,y32)
+            direction = normalize(distance(quadcopterc[0],quadcopterc[1],x32,y32))
+            quadcopterc[0] += speed*direction[0]
+            quadcopterc[1] += speed*direction[1]
         else:
-            direction = normalize(distance(quadcopter.centerx,quadcopter.centery,x31,y31))
-            quadcopter.centerx += speed*direction[0]
-            quadcopter.centery += speed*direction[1]
+            (x31,y31) = updatePoint(x31,y31)
+            direction = normalize(distance(quadcopterc[0],quadcopterc[1],x31,y31))
+            quadcopterc[0] += speed*direction[0]
+            quadcopterc[1] += speed*direction[1]
 
     else:
+        direction = normalize(distance(quadcopterc[0],quadcopterc[1],goal[0],goal[1]))
+        quadcopterc[0] += speed*(direction[0])
+        quadcopterc[1] += speed*(direction[1])
 
-        direction = normalize(distance(quadcopter.centerx,quadcopter.centery,goal[0],goal[1]))
-        quadcopter.centerx += speed*(direction[0])
-        quadcopter.centery += speed*(direction[1])
-
+    quadcopter.center = quadcopterc
     screen.fill(black)
     screen.blit(ball, quadcopter)
-    screen.blit(ball2, tree)
-    pygame.draw.circle(screen, (0, 127, 255), tree.center, int(R), 1)
+    screen.blit(pic, tree)
+    for obstacle in obstacles:
+        screen.blit(pic,obstacle[0])
+        pygame.draw.circle(screen, (0, 127, 255), obstacle[0].center, int(R), 2)
+
     pygame.draw.circle(screen, (0, 127, 255), goal, 2, 1)
     pygame.draw.line(screen, (70, 127, 40), quadcopter.center, goal, 2)
-    pygame.draw.line(screen, (70, 127, 255), quadcopter.center, (x31, y31), 2)
-    pygame.draw.line(screen, (70, 127, 255), quadcopter.center, (x32, y32), 2)
+    pygame.draw.line(screen, (70, 20, 100), quadcopter.center, (x31, y31), 2)
+    pygame.draw.line(screen, (70, 20, 100), quadcopter.center, (x32, y32), 2)
+
+   # print(direction[0]*30+quadcopter.centerx, direction[1]*30+quadcopter.centery)
+    pygame.draw.line(screen, (170, 67, 160), (0,0), (direction[0]*300, direction[1]*300), 2)
     pygame.display.flip()
+    pygame.time.wait(2)
+   # if(math.sqrt(math.pow(quadcopter.centerx - goal[0], 2) + math.pow(quadcopter.centery - goal[1], 2)) <2):
+    #    os.system("pause")
 
 
-     #greedyGoal = int(math.sqrt(math.fabs(math.pow((x0-x1), 2), math.pow((y0-y1), 2))))
-     
 
 
 
