8a0f8f43926ea21e2d34839fa5c85c0d70af97b6
Martin Asenov <masenov@uci.edu>
2014-11-28 13:57:57 -0800

    Circle Line intersection BUGFIX
    The previous algorithm used was for circle-line intersection. The new
    algorithm is for circle-line segment intersection.

--- AI and CV/Very Temporary Obstacle Avoidance/game.py
+++ AI and CV/Very Temporary Obstacle Avoidance/game.py
@@ -5,18 +5,25 @@ from pygame.locals import *
 import math
 import numpy as np
 
+
+# Distance between two points
 def pointDistance(p1,p2):
     return math.sqrt(math.pow(p1[0]-p2[0],2) + math.pow(p1[1]-p2[1],2))
 
+# Distance between coordinates of two points
 def distance(x1,y1,x2,y2):
     distx = x2 - x1
     disty = y2 - y1
     return (distx, disty)
 
-def length(vector): # get length (used for normalize)
+def dotProduct(v1,v2):
+    return v1[0]*v2[0] + v1[1]*v2[1]
+
+# Length of a vector (used for normalize)
+def length(vector):
     return math.sqrt((vector[0]**2 + vector[1]**2))
 
-
+# Normalizing a vector
 def normalize(vector): # turn a total value into a constant amount (hard to describe)
                          # (0,50) becomes(0.0,1.0)
                          # (50,50) becomes (0.707, 0.707)
@@ -27,20 +34,32 @@ def normalize(vector): # turn a total value into a constant amount (hard to desc
 
 
 
+# Check if lines intersects with a circle - http://doswa.com/2009/07/13/circle-segment-intersectioncollision.html
 
-def calculateDelta(obstacle):
-    # We have to subtract tree x and y because our circle does not have coordinates (0,0)
-    px1 = quadcopterc[0] - obstacle.centerx
-    py1 = quadcopterc[1] - obstacle.centery
-    px2 = goal[0] - obstacle.centerx
-    py2 = goal[1] - obstacle.centery
-    dx = px2 - px1
-    dy = py2 - py1
-    dr = math.sqrt(math.pow(dx,2) + math.pow(dy,2))
-    D2 = px1*py2 - px2*py1
-    delta = R*R*dr*dr - D2*D2
-    return delta
+def closest_point_on_seg(seg_a, seg_b, circ_pos):
+    seg_v = distance(seg_a[0],seg_a[1],seg_b[0],seg_b[1])
+    pt_v = distance(seg_a[0],seg_a[1],circ_pos[0],circ_pos[1])
+    if length(seg_v) <= 0:
+        raise ValueError, "Invalid segment length"
+    seg_v_unit = [i/length(seg_v) for i in seg_v]
+    proj = dotProduct(pt_v,seg_v_unit)
+    if proj <= 0:
+        return seg_a
+    if proj >= length(seg_v):
+        return seg_b
+    proj_v = [i * proj for i in seg_v_unit]
+    closest = [proj_v[0]+seg_a[0], proj_v[1] + seg_a[1]]
+    return closest
 
+def segment_circle(seg_a, seg_b, circ_pos, circ_rad):
+    closest = closest_point_on_seg(seg_a, seg_b, circ_pos)
+    dist_v = [circ_pos[0] - closest[0],circ_pos[1] - closest[1]]
+    if length(dist_v) <= circ_rad:
+        return True
+    else:
+        return False
+
+# Calculate tangent lines between the quadcopter and the a specific obstacle
 def calculateTangentLines(obstacle):
     x1 = obstacle.centerx
     y1 = obstacle.centery
@@ -56,14 +75,14 @@ def calculateTangentLines(obstacle):
     y32 = y2 - h * (x1 - quadcopterc[0]) / D
     return (x31,y31,x32,y32)
 
-
+# Update point
 def updatePoint(x,y):
     try:
-        x += (x - quadcopter.centerx)/length([x-quadcopter.centerx,y-quadcopter.centery])
+        x += (x - quadcopterc[0])/length([x-quadcopterc[0],y-quadcopterc[1]])
     except ZeroDivisionError:
         pass
     try:
-        y += (y - quadcopter.centery)/length([x-quadcopter.centerx,y-quadcopter.centery])
+        y += (y - quadcopterc[1])/length([x-quadcopterc[0],y-quadcopterc[1]])
     except ZeroDivisionError:
         pass
     return (x,y)
@@ -76,17 +95,16 @@ screen = pygame.display.set_mode((1200, 800))
 size = width, height = 1200, 800
 
 black = 250, 250, 250
-goal = (800, 400)
+goal = (1000, 700)
 ball = pygame.image.load("ball.bmp")
 quadcopter = ball.get_rect()
 quadcopterc = [quadcopter.centerx, quadcopter.centery]
-
-pic = pygame.image.load("tree2.png")
+pic = pygame.image.load("tree3.png")
 tree = pic.get_rect()
 tree = tree.move(-tree.width/2,-tree.height/2)
-R = tree.width/2*math.sqrt(2)
+R = tree.width/2*math.sqrt(2) + 30
 
-points = [(400,150),(300,200),(1000,700),(300,700),(500,400)]
+points = [(100,200),(300,200),(500,200),(300,700),(800,350),(700,450),(600,550),(900,250)]
 obstacles = []
 obstacles_bucket = []
 
@@ -127,7 +145,7 @@ for bucket in obstacles:
 
 
 direction=([1,1])
-speed = 7
+speed = 2
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
@@ -137,9 +155,8 @@ while 1:
     min_obstacle = obstacles_bucket[0][0]
     for bucket in obstacles_bucket:
         for obstacle in bucket:
-            # delta = calculateDelta(obstacle)
-            delta = calculateDelta(obstacle)
-            if(delta>0 and distance(quadcopterc[0], quadcopterc[1], obstacle.centerx, obstacle.centery) < distance(quadcopterc[0],quadcopterc[1],goal[0],goal[1])):
+            delta = segment_circle(quadcopterc,goal,obstacle.center,R)
+            if(delta):
                 delta2 = 1
                 distanc = pointDistance(obstacle, quadcopterc)
                 if(distanc<min_distance):
@@ -164,10 +181,10 @@ while 1:
         for tangent2 in tangents:
             if(tangent1==tangent2):
                 continue
-            u1 = tangent1[0] - quadcopter.centerx
-            u2 = tangent1[1] - quadcopter.centery
-            v1 = tangent2[0] - quadcopter.centerx
-            v2 = tangent2[1] - quadcopter.centery
+            u1 = tangent1[0] - quadcopterc[0]
+            u2 = tangent1[1] - quadcopterc[1]
+            v1 = tangent2[0] - quadcopterc[0]
+            v2 = tangent2[1] - quadcopterc[1]
             cos = (u1*v1 + u2*v2)/((math.sqrt(math.pow(u1,2) + math.pow(u2, 2)))*(math.sqrt(math.pow(v1,2) + math.pow(v2,2))))
             if (cos<angle):
                 angle = cos
@@ -224,10 +241,8 @@ while 1:
 
    # print(direction[0]*30+quadcopter.centerx, direction[1]*30+quadcopter.centery)
     pygame.display.flip()
-    pygame.time.wait(22)
    # if(math.sqrt(math.pow(quadcopter.centerx - goal[0], 2) + math.pow(quadcopter.centery - goal[1], 2)) <2):
     #    os.system("pause")
-
 
 
 
